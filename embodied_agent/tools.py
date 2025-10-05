2f4af85c66df02a0dedadf06567fccefb2bb94cd
Abhishek <deshmukhabhi02@gmail.com>
2025-10-05 18:16:24 +0200

    #2: Minor changes

--- embodied_agent/tools.py
+++ embodied_agent/tools.py
@@ -1,9 +1,36 @@
 from langchain_core.tools import tool
 
-@tool
 def multiply(a: int, b: int) -> int:
-    """Multiply two numbers."""
+    """Multiply a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
     return a * b
 
+
+
+def add(a: int, b: int) -> int:
+    """Adds a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a + b
+
+
+def divide(a: int, b: int) -> float:
+    """Divide a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a / b
+
 def get_tools() -> list:
-    return [multiply]
+    return [multiply,
+            add,
+            divide]
